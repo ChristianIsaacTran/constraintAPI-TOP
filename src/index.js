5186b7ab87be0581e58144b95808d25b9cf3c6e2
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-05-22 00:52:19 -0500

    Added email validation, works. Also added required
    Added working validation for the email
    element using regex, and also used the
    .reportValidity to show the user if the
    current input is non-valid. Also put required
    in HTML and made a visual indicator with directions
    on what required elements look like. The
    novalidate property doesn't affect the
    "required" property's ability to trigger
    the invalid/valid state, so the CSS still works.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css";
+import validation from "./modules/validation.js";
 
+const formValidation = validation();
+
+const emailInput = document.querySelector("#email");
+
+formValidation.createEmailValidation(emailInput);
---
+++ src/modules/validation.js
@@ -0,0 +1,30 @@
+function validation() {
+    // email validation
+    function checkEmail(emailVal) {
+        const emailFormat = /^[\w-.]+@([\w-]+.)+[\w-]{2,}$/;
+
+        if (emailFormat.test(emailVal)) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    function createEmailValidation(emailInput) {
+        emailInput.addEventListener("input", () => {
+            console.log("fired");
+            // if invalid email input, then mark invalid. Clear if valid
+            if(!checkEmail(emailInput.value)) {
+                emailInput.setCustomValidity("Enter a valid email address in format: something@company.com")
+            } else { 
+                emailInput.setCustomValidity("");
+            }
+
+            emailInput.reportValidity();
+        });
+    }
+
+    return { createEmailValidation };
+}
+
+export default validation;
